755dcfe2f5747d160ea428df946307b3f9661d5b
devmrko <jmko09@hist.co.kr>
2019-04-25 10:46:44 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+})
+
+router.get('/login',function(req,res){
+  res.sendFile(path.join(__dirname+'/login.html'));
+  //__dirname : It will resolve to your project folder.
+})
+
+app.use(express.static(__dirname + '/asset'));
+//Store all JS and CSS in Scripts folder.
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
